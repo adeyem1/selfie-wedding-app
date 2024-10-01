93e1cb8fc5307c23c8f37b4569fbcdff0dde078f
adeyem1 <adeyemiadesanya399@gmail.com>
2024-10-01 16:34:31 +0100

    added the mobile and web main image

--- src/App.jsx
+++ src/App.jsx
@@ -70,93 +70,84 @@ function App() {
 
   return (
     <div className="App">
-    <div
- className="flex justify-center items-center h-screen bg-mobile-bg sm:bg-desktop-bg bg-cover bg-center"
->
-  {/* Navbar */}
-  <nav className=" name absolute top-0 w-full z-50 flex justify-between items-center px-6 py-4 text-white bg-transparent">
-    <div className="text-lg font-bold">
-      {name1} <span className="text-pink-400">❤️</span> {name2}
-    </div>
-    <ul className="flex space-x-6 text-sm sm:text-base">
-      <li>
-        <a
-          href="#camera"
-          className="hover:text-pink-400 hover:border-b-2 hover:border-pink-400 transition duration-300 ease-in-out"
-        >
-          Camera
-        </a>
-      </li>
-      <li>
-        <a
-          href="#gallery"
-          className="hover:text-pink-400 hover:border-b-2 hover:border-pink-400 transition duration-300 ease-in-out"
-        >
-          Gallery
-        </a>
-      </li>
-    </ul>
-  </nav>
+      <div
+        className="flex justify-center items-center h-screen bg-mobile-bg sm:bg-desktop-bg bg-cover bg-center"
+      >
+        {/* Navbar */}
+        <nav className=" name absolute top-0 w-full z-50 flex justify-between items-center px-6 py-4 text-white bg-transparent">
+          <div className="text-lg font-bold">
+            {name1} <span className="text-pink-400">❤️</span> {name2}
+          </div>
+          <ul className="flex space-x-6 text-sm sm:text-base">
+            <li>
+              <a
+                href="#camera"
+                className="hover:text-pink-400 hover:border-b-2 hover:border-pink-400 transition duration-300 ease-in-out"
+              >
+                Camera
+              </a>
+            </li>
+            <li>
+              <a
+                href="#gallery"
+                className="hover:text-pink-400 hover:border-b-2 hover:border-pink-400 transition duration-300 ease-in-out"
+              >
+                Gallery
+              </a>
+            </li>
+          </ul>
+        </nav>
 
-  {/* Main Content */}
-  <div className="text-center  ">
-    <h1 className="name2 text-4xl sm:text-5xl md:text-6xl font-bold  bg-opacity-50">
-    Welcome to Our Wedding Selfie Booth! 📸💍
-    </h1>
+        {/* Main Content */}
+        <div className="text-center  ">
+          <h1 className="name2 text-4xl sm:text-5xl md:text-6xl font-bold  bg-opacity-50">
+            Welcome to Our Wedding Selfie Booth! 📸💍
+          </h1>
 
-    <p className=" name3 mt-4 text-lg sm:text-xl md:text-2xl p-4 ">
-    We’re excited to celebrate with you! Capture the love, laughter, and fun moments. Snap your memories and be part of our special day!
-</p>
+          <p className=" name3 mt-4 text-lg sm:text-xl md:text-2xl p-4 ">
+            We’re excited to celebrate with you! Capture the love, laughter, and fun moments. Snap your memories and be part of our special day!
+          </p>
 
-    <p className="mt-2 text-xl sm:text-2xl md:text-3xl text-black font-light">
-     
-    </p>
+          <div className="mt-4 border-t-2 border-rose-200 w-16 mx-auto"></div>
+        </div>
+      </div>
 
-    <div className="mt-4 border-t-2 border-rose-200 w-16 mx-auto"></div>
-  </div>
-</div>
-
-<div className="  p-6 mb-4 bg-slate-100">
-  <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">How It Works:</h1>
-  <p className="mt-4 text-lg sm:text-xl md:text-2xl">
-<ul>
-  <li><b>Snap a Selfie: </b>Grab your phone, find your best angle, and take a selfie at the wedding. </li>
-  <li> <b>Upload & Share: </b>Use the camera icon to upload your photo and become part of our digital wedding album.</li>
-  <li><b>Spread the Love:</b> Feel free to add a fun message or tag your friends in the gallery. <br />
-  Let&apos;s Create Memories Together!</li>
-</ul>
-
-</p>
-<p className="mt-4 text-lg sm:text-xl md:text-2xl">From the silly moments to the glamorous poses, every picture tells a story. So let’s fill this space with the best memories of our special day!
-</p>
-</div>
-
+      <div className="  p-6 mb-4 bg-slate-100">
+        <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">How It Works:</h1>
+        <p className="mt-4 text-lg sm:text-xl md:text-2xl">
+          <ul>
+            <li><b>Snap a Selfie: </b>Grab your phone, find your best angle, and take a selfie at the wedding. </li>
+            <li> <b>Upload & Share: </b>Use the camera icon to upload your photo and become part of our digital wedding album.</li>
+            <li><b>Spread the Love:</b> Feel free to add a fun message or tag your friends in the gallery.<br /> Let's Create Memories Together!</li>
+          </ul>
+        </p>
+        <p className="mt-4 text-lg sm:text-xl md:text-2xl">From the silly moments to the glamorous poses, every picture tells a story. So let’s fill this space with the best memories of our special day!
+        </p>
+      </div>
 
       {/* Camera Section */}
       <h1 id="camera" className="text-4xl sm:text-5xl md:text-6xl font-bold mt-4 text-center">Camera</h1>
       <p className="border-t-2 border-pink-400 w-16 mx-auto mb-4"></p>
 
       <div className="flex flex-col items-center justify-center h-screen bg-white-100">
-      <div className="relative flex-grow w-full flex items-center justify-center">
-      <div className="border-4 border-gray-300 rounded-lg shadow-lg overflow-hidden w-full h-full max-w-screen-lg max-h-screen-lg">
-    <Webcam
-  ref={webcamRef}
-  screenshotFormat="image/jpeg"
-  mirrored={true}
-  className="w-full h-full max-w-screen-lg max-h-screen-lg object-cover aspect-video"
-/>
-
-      {/* Capture Image Button */}
-      <button
-        onClick={captureImage}
-        className="absolute bottom-4 left-1/2 transform -translate-x-1/2 w-12 h-12 sm:w-16 sm:h-16 bg-pink-100 text-white font-semibold rounded-full shadow-lg hover:bg-pink-400 transition duration-300 ease-in-out flex items-center justify-center"
-      >
-        <span className="sr-only">Capture Image</span>
-      </button>
-    </div>
-  </div>
-</div>
-
+        <div className="relative flex-grow w-full flex items-center justify-center">
+          <div className="border-4 border-gray-300 rounded-lg shadow-lg overflow-hidden w-full h-full max-w-screen-lg max-h-screen-lg">
+            <Webcam
+              ref={webcamRef}
+              screenshotFormat="image/jpeg"
+              mirrored={true}
+              className="w-full h-full max-w-screen-lg max-h-screen-lg object-cover"
+            />
+            {/* Capture Image Button */}
+            <button
+              onClick={captureImage}
+              className="absolute bottom-4 left-1/2 transform -translate-x-1/2 w-12 h-12 sm:w-16 sm:h-16 bg-pink-100 text-white font-semibold rounded-full shadow-lg hover:bg-pink-400 transition duration-300 ease-in-out flex items-center justify-center"
+            >
+              <span className="sr-only">Capture Image</span>
+            </button>
+          </div>
+        </div>
+      </div>
 
       {/* Preview captured webcam image in a pop-up */}
       {capturedImage && (
